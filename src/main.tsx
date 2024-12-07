0d02eafe80882fd7163af837156ec9567ec7fb46
Jon Rey Galera <jonreygalera@gmail.com>
2024-12-07 17:05:43 +0800

    reverted

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import RedirectPage from './pages/RedirectPage'
+import RootRoute from './routes/RootRoute'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense>
-      <RedirectPage/>
+      <RootRoute/>
     </Suspense>
   </StrictMode>,
 )
